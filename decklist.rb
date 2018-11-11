bdbabfb306fd11ebfb5f5b0d63704fd1a34b4399
Brendan Tang <brndntng@gmail.com>
2018-11-11 15:09:48 -0500

    Decklist class

--- decklist.rb
+++ decklist.rb
@@ -16,24 +16,41 @@ class Card
   end
 end
 
-
-def get_card_data(decklist_array)
-  identifiers_array = []
-  decklist_array.each do |name|
-    identifiers_array << {"name" => name}
+class Decklist
+  attr_accessor :cards
+  def initialize(array)
+    @array = array
+    @cards = {}
+    self.get_card_data 
   end
   
-  query_hash = {"identifiers" => identifiers_array}
-  
-  json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
-  uri = URI.parse("https://api.scryfall.com/cards/collection")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
+  def get_card_data
+    identifiers_array = []
+    @array.each do |name|
+      identifiers_array << {"name" => name}
+    end
+    
+    query_hash = {"identifiers" => identifiers_array}
+    
+    json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
+    uri = URI.parse("https://api.scryfall.com/cards/collection")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
 
-  response = http.post(uri.path, query_hash.to_json, json_headers)
-  card_data_hash = JSON.parse(response.body)
+    response = http.post(uri.path, query_hash.to_json, json_headers)
+    response_hash = JSON.parse(response.body)
+    @cards = response_hash["data"]
+  end
+  
+  def display
+    @cards.each do |card|
+      string = "#{card["name"]}  #{card["mana_cost"]}  #{card["usd"]}"
+      puts string
+    end
+  end
 end
 
 decklist_array = ["lightning bolt", "mountain", "lava spike", "searing blaze", "eidolon of the great revel", "searing blood", "monastery swiftspear", "bedlam reveler", "harsh mentor", "bomat courier", "thoughtseize"]
 
-pp get_card_data(decklist_array)
+decklist = Decklist.new(decklist_array)
+decklist.display
