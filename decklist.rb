6b2743a5ff55d5d196d75207b19561ca62f14f45
Brendan Tang <brndntng@gmail.com>
2018-11-11 12:45:32 -0500

    fetch and display a name from scryfall

--- decklist.rb
+++ decklist.rb
@@ -1,1 +1,11 @@
 # Read plaintext decklist and output pretty version
+
+require 'net/http'
+require 'json'
+require 'pp'
+
+fireball_uri = URI('https://api.scryfall.com/cards/named?fuzzy=fireball')
+fireball_json = Net::HTTP.get(fireball_uri)
+fireball = JSON.parse(fireball_json)
+
+puts fireball["name"]
